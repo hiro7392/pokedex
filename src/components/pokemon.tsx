67650e6b66947695f45b837c7ad28c2881e4c017
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2024-02-25 21:09:49 +0900

    get pk img

--- src/components/pokemon.tsx
+++ src/components/pokemon.tsx
@@ -1,23 +1,63 @@
-export const Pokemon=()=>{
+import { MainClient } from "pokenode-ts";
+import { Suspense, useEffect, useState } from "react";
+import useSWR from "swr";
+import { pokemonNameMap, toLowerCase } from "../data/mapNameLanguage";
+import { toJapaneseType } from "../data/pokemonTypeMap";
 
-
-    const handleClick=()=>{
-        const res=fetch('https://pokeapi.co/api/v2/pokemon/150',{method:'GET'})
-        console.log(res);
-        res.then((json)=>{
-            console.log(json)
-        }).catch(
-
-        ).finally(
-
-        )
-    }
-
-    return(
-        <>
-            <button onClick={handleClick}>
-                get pokemon
-            </button>
-        </>
-    )
+function Loading() {
+  return <h2>🌀 Loading...</h2>;
 }
+type pokemonType={
+  type1:string | null,
+  type2:string | null  
+}
+
+export const Pokemon = () => {
+  const [imgURL, setImgURL] = useState<string | null>(null);
+  const [count,setCount] =useState<number>(0);
+  const {data:pokemonData} = useSWR(toLowerCase(pokemonNameMap[count].en), () =>
+  api.pokemon.getPokemonByName(toLowerCase(pokemonNameMap[count].en))
+  );
+  const [types,setTypes]=useState<pokemonType>({type1:"none",type2:"none"});
+  const api = new MainClient();
+  
+
+  const handleClick = () => {
+    setImgURL(pokemonData?.sprites.front_default ?? null);
+  };
+  useEffect(()=>{
+    setImgURL(pokemonData?.sprites.front_default ?? null);
+    setTypes({
+      type1: toJapaneseType(pokemonData?.types[0]?.type?.name) ?? "none",
+      type2: toJapaneseType(pokemonData?.types[1]?.type?.name) ?? "none"
+    });
+  },[count,pokemonData])
+
+  return (
+    <>
+      <Suspense fallback={<Loading/>}>
+        <div style={{minHeight:"130px"}}>
+          <img src={imgURL ?? ""} style={{minHeight:"200px"}}/>
+        </div>
+      </Suspense>
+      {
+        count > 0 ? 
+        <button onClick={() => setCount((count) => count - 1)}>
+        {"<"}
+        </button>
+        : null
+      }
+      {
+        count < pokemonNameMap.length
+        ?<button onClick={() => setCount((count) => count + 1)}>
+        {">"}
+        </button>
+        : null
+      }
+      <div>
+        <p>タイプ<br></br>{types.type1=="none"?null:types.type1} {types.type2=="none"?null:"・"+types.type2}</p>
+      </div>
+      <button onClick={handleClick}>get pokemon</button>
+    </>
+  );
+};
---
+++ src/data/pokemonTypeMap.ts
@@ -0,0 +1,79 @@
+export const pokemonTypesMap = [
+    {
+        en:"normal",
+        ja:"ノーマル"
+    },
+    {
+        en:"fire",
+        ja:"ほのお"
+    },
+    {
+        en:"water",
+        ja:"みず"
+    },
+    {
+        en:"grass",
+        ja:"くさ"
+    },
+    {
+        en:"electric",
+        ja:"電気"
+    },
+    {
+        en:"ice",
+        ja:"こおり"
+    },
+    {
+        en:"fighting",
+        ja:"かくとう"
+    },
+    {
+        en:"poison",
+        ja:"どく"
+    },
+    {
+        en:"ground",
+        ja:"じめん"
+    },
+    {
+        en:"flying",
+        ja:"ひこう"
+    },
+    {
+        en:"psychic",
+        ja:"エスパー"
+    },
+    {
+        en:"bug",
+        ja:"むし"
+    },
+    {
+        en:"rock",
+        ja:"いわ"
+    },
+    {
+        en:"ghost",
+        ja:"ゴースト"
+    },
+    {
+        en:"dragon",
+        ja:"ドラゴン"
+    },
+    {
+        en:"dark",
+        ja:"あく"
+    },
+    {
+        en:"steel",
+        ja:"はがね"
+    },
+    {
+        en:"fairy",
+        ja:"フェアリー"
+    }
+]
+export const toJapaneseType = (enType: string | undefined) => {
+
+    const types= pokemonTypesMap.find(({ en }) => en === enType);
+    return types?.ja
+}
