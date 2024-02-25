8b2d16584d5cc10d2a4136526aca96827f4c450a
sakana7392 <k_hiro@kzd.biglobe.ne.jp>
2024-02-25 23:53:04 +0900

    fix App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { Pokemon } from "./components/pokemon";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { Pokemon } from './components/pokemon'
+
 
 function App() {
-
   return (
     <>
-      <Pokemon />
+      <h2>ポケモン図鑑</h2>
+      <Pokemon/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
